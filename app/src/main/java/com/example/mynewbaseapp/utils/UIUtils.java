f49968773739fb6e33ed610dc374d85812cd404e
tangzhaoqiang <tangzhaoqiang@doov.com.cn>
2017-10-21 15:17:43 +0800

    封装了popupWindow，并添加了判断屏幕大小方法

--- app/src/main/java/com/example/mynewbaseapp/utils/UIUtils.java
+++ app/src/main/java/com/example/mynewbaseapp/utils/UIUtils.java
@@ -372,4 +372,15 @@ public class UIUtils {
         return (pxVal / context.getResources().getDisplayMetrics().scaledDensity);
     }
 
+    /**
+     * 测量View的宽高
+     *
+     * @param view View
+     */
+    public static void measureWidthAndHeight(View view) {
+        int widthMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        int heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED);
+        view.measure(widthMeasureSpec, heightMeasureSpec);
+    }
+
 }
